7afdca91d0d670356d14ff9f03a93cf0ee7cb564
matasho <matasho2000@yahoo.com>
2016-12-03 08:05:44 -0800

    test nfc

--- app/src/main/java/aero/panasonic/nfcreader/MainActivity.java
+++ app/src/main/java/aero/panasonic/nfcreader/MainActivity.java
@@ -1,14 +1,39 @@
 package aero.panasonic.nfcreader;
 
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final byte[] ENGLISH_PLAIN_TEXT =
+            new byte[] {(byte) 0xd1, (byte) 0x01, (byte) 0x1c, (byte) 0x54, (byte) 0x02, (byte) 0x65,
+                    (byte) 0x6e, (byte) 0x53, (byte) 0x6f, (byte) 0x6d, (byte) 0x65, (byte) 0x20,
+                    (byte) 0x72, (byte) 0x61, (byte) 0x6e, (byte) 0x64, (byte) 0x6f, (byte) 0x6d,
+                    (byte) 0x20, (byte) 0x65, (byte) 0x6e, (byte) 0x67, (byte) 0x6c, (byte) 0x69,
+                    (byte) 0x73, (byte) 0x68, (byte) 0x20, (byte) 0x74, (byte) 0x65, (byte) 0x78,
+                    (byte) 0x74, (byte) 0x2e};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test
+
+        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if(nfcAdapter == null)
+            Log.v(TAG, "NFC not available on device");
     }
+
+    public void sendMessage(View view){
+        try {
+            NdefMessage[] msg = new NdefMessage[]{new NdefMessage(ENGLISH_PLAIN_TEXT)};
+        } catch(final Exception e){
+            throw new RuntimeException("Cannot parse data", e);
+        }
+    }
+
+
 }
