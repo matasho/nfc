a91ddebff1430c71ecee18775c33a7e20eb1fd8b
matasho <matasho2000@yahoo.com>
2016-12-04 07:58:49 -0800

    added some byte items

--- app/src/main/java/aero/panasonic/nfcreader/MainActivity.java
+++ app/src/main/java/aero/panasonic/nfcreader/MainActivity.java
@@ -1,21 +1,20 @@
 package aero.panasonic.nfcreader;
 
+import android.content.Intent;
 import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import java.nio.charset.Charset;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
-    private static final byte[] ENGLISH_PLAIN_TEXT =
-            new byte[] {(byte) 0xd1, (byte) 0x01, (byte) 0x1c, (byte) 0x54, (byte) 0x02, (byte) 0x65,
-                    (byte) 0x6e, (byte) 0x53, (byte) 0x6f, (byte) 0x6d, (byte) 0x65, (byte) 0x20,
-                    (byte) 0x72, (byte) 0x61, (byte) 0x6e, (byte) 0x64, (byte) 0x6f, (byte) 0x6d,
-                    (byte) 0x20, (byte) 0x65, (byte) 0x6e, (byte) 0x67, (byte) 0x6c, (byte) 0x69,
-                    (byte) 0x73, (byte) 0x68, (byte) 0x20, (byte) 0x74, (byte) 0x65, (byte) 0x78,
-                    (byte) 0x74, (byte) 0x2e};
+    private static final byte[] ENGLISH_PLAIN_TEXT = new byte[] {(byte) 0xd1, (byte) 0x01, (byte) 0x08, (byte) 0x55, (byte) 0x01, (byte) 0x63, (byte) 0x6e, (byte) 0x6e, (byte) 0x2e, (byte) 0x63, (byte) 0x6f, (byte) 0x6d};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +27,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendMessage(View view){
+        final Intent intent = new Intent(NfcAdapter.ACTION_TAG_DISCOVERED);
+        NdefMessage[] msg;
+
         try {
-            NdefMessage[] msg = new NdefMessage[]{new NdefMessage(ENGLISH_PLAIN_TEXT)};
+            msg = new NdefMessage[]{new NdefMessage(ENGLISH_PLAIN_TEXT)};
         } catch(final Exception e){
             throw new RuntimeException("Cannot parse data", e);
         }
+
+        intent.putExtra(NfcAdapter.EXTRA_NDEF_MESSAGES, msg);
+
+        createRecord();
+    }
+
+    private void createRecord(){
+        NdefRecord record = NdefRecord.createMime("text/plain", ENGLISH_PLAIN_TEXT);
+        System.out.println(Arrays.toString(record.getPayload()));
+    }
+
+    @Override
+    public void onNewIntent(Intent intent){
+
     }
 
 
